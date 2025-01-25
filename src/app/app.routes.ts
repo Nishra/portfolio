0cc6998ba2f2fb936f5e495cbaef31113e91867d
Nishra Nasar <nishra.nasar@schoolgrid.co.uk>
2025-01-25 17:43:26 +0530

    feat: add app component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent }, // Default route for the home page
+  { path: 'about', component: HomeComponent }, // Use HomeComponent but navigate to the "About" section
+  { path: 'skills', component: HomeComponent }, // Navigate to the "Skills" section
+  { path: 'education', component: HomeComponent }, // Navigate to the "Education" section
+  { path: 'projects', component: HomeComponent }, // Navigate to the "Projects" section
+  { path: 'contact', component: HomeComponent }, // Navigate to the "Contact" section
+];
